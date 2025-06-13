92f261bd560bbe73a4de953851bb00d46b51fc1d
Arda <bozkurtarda59@gmail.com>
2025-06-13 20:11:39 +0300

    Convert next.config.ts to next.config.js

---
+++ next.config.js
@@ -0,0 +1,9 @@
+// next.config.js
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  // your Next.js config options here
+  reactStrictMode: true,
+  // ... other configs
+};
+
+module.exports = nextConfig;
